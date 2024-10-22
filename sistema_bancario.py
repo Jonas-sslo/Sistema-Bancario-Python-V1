2b1c04b224c44a10ec51a896f0f9d16cb9e596d8
Jonas Sanjinez Lopes <batmancarecazz@gmail.com>
2024-10-21 22:39:44 -0300

    Adicionando logs no extrato

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,86 +1,102 @@
+from datetime import datetime
+
 saldo = 100
-contagem_saques = 0
-LIMITE_SAQUES = 3
 saques = []
 depositos = []
+limite_transacoes = []
+horarios_transacoes = []
 
 
-def depositar(saldo, valor):
+def atualizar_limite_transacoes():
+    return len(depositos) + len(saques)
+
+
+def define_horario_transacoes():
+    return datetime.now()
+
+
+def depositar(saldo, limite_transacoes, horarios_transacoes, valor):
     if valor <= 0:
         print("\nOperação cancelada! Não foi possível depositar a quantia informada.")
+    elif atualizar_limite_transacoes() >= 10:
+        print("\nSeu limite de saques e depósitos diários foi atingido!")
     else:
         saldo += valor
         depositos.append(valor)
+        horarios_transacoes.append(define_horario_transacoes())
+        limite_transacoes = atualizar_limite_transacoes()
         print(
-            f"\nForam inseridos R${valor:.2f} na sua conta \nSaldo atual: R${saldo:.2f}"
+            f"\nForam inseridos R${valor:.2f} na sua conta \n\nSaldo atual: R${saldo:.2f} \n \nForam realizadas {limite_transacoes} transações até agora!"
         )
-    return saldo
+    return saldo, limite_transacoes, horarios_transacoes
 
 
-def sacar(saldo, contagem_saques, valor):
+def sacar(saldo, limite_transacoes, horarios_transacoes, valor):
     if valor <= 0:
         print("\nOperação cancelada! Não foi possível sacar a quantia informada.")
-    elif contagem_saques >= LIMITE_SAQUES:
-        print("\nSeu limite de saques diários foi atingido!")
+    elif atualizar_limite_transacoes() >= 10:
+        print("\nSeu limite de saques e depósitos diários foi atingido!")
     elif saldo < valor:
         print("\nOperação cancelada! Você não possui saldo!")
     else:
         if valor <= 500:
             saldo -= valor
-            contagem_saques += 1
             saques.append(valor)
+            horarios_transacoes.append(define_horario_transacoes())
+            limite_transacoes = atualizar_limite_transacoes()
             print(
-               f"\nForam sacados R${valor:.2f} da sua conta \nSaldo atual: R${saldo:.2f} \n \nForam realizados {contagem_saques} até agora!"
+                f"\nForam sacados R${valor:.2f} da sua conta \n\nSaldo atual: R${saldo:.2f} \n \nForam realizadas {limite_transacoes} transações até agora!"
             )
         else:
             print("\nOperação cancelada! O seu limite de saque é de R$500,00")
-    return saldo, contagem_saques
+    return saldo, limite_transacoes, horarios_transacoes
 
 
-def extrato(depositos, saques, saldo):
-    if (not depositos) and (not saques):
+def extrato(depositos, saques, horario_transacoes, saldo):
+    if not depositos and not saques:
         print("\nSeu extrato está vazio!")
     else:
         if depositos:
             print("\nSeus depósitos:")
-            for deposito in depositos:
-                print(f"R${deposito:.2f}")
+            for i, deposito in enumerate(depositos):
+                print(
+                    f"R${deposito:.2f} \nHorário do Depósito: {horarios_transacoes[i]}\n"
+                )
         if saques:
             print("\nSeus saques:")
-            for saque in saques:
-                print(f"R${saque:.2f}")
-
+            for i, saque in enumerate(saques):
+                print(
+                    f"R${saque:.2f} \nHorário do Saque: {horarios_transacoes[len(depositos) + i]}\n"
+                )
         if not depositos:
             print("\nAinda não foram realizados depósitos")
         if not saques:
             print("\nAinda não foram realizados saques")
-
     print(f"\nSaldo atual: R${saldo:.2f}")
 
 
 while True:
-
     print(
-        f""" 
-========== MENU ==========
+        """\n========== MENU ==========
 [1] - Realizar Deposito
 [2] - Realizar Saque
 [3] - Visualizar Extrato
 [0] - Sair
-==========================
-    """
+=========================="""
     )
-
-    opcao = int(input(f"Escolha uma opção: "))
-
+    opcao = int(input("Escolha uma opção: "))
     if opcao == 1:
         valorDeposito = float(input("\nInsira o valor a ser depositado: "))
-        saldo = depositar(saldo, valorDeposito)
+        saldo, limite_transacoes, horarios_transacoes = depositar(
+            saldo, limite_transacoes, horarios_transacoes, valorDeposito
+        )
     elif opcao == 2:
         valorSaque = float(input("\nInsira o valor a ser sacado: "))
-        saldo, contagem_saques= sacar(saldo, contagem_saques, valorSaque)
+        saldo, limite_transacoes, horarios_transacoes = sacar(
+            saldo, limite_transacoes, horarios_transacoes, valorSaque
+        )
     elif opcao == 3:
-        extrato(depositos, saques, saldo)
+        extrato(depositos, saques, horarios_transacoes, saldo)
     elif opcao == 0:
         print("Operação encerrada! Saindo do programa...")
         break
